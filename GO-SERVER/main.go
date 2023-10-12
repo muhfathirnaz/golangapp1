621538d0985e84857fe5580c38534461bf983c58
muhfathirnaz <122539375+muhfathirnaz@users.noreply.github.com>
2023-10-12 16:44:12 +0700

    12/10/2023

---
+++ GO-SERVER/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.newRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn: "438227", Title: "Movie One", Director: &Director{Firstname: "John", Lastname: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "4381421", Title: "Movie Two", Director: &Director{Firstname: "Fathir", Lastname: "Muhammad"}})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("starting server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
+
+}
